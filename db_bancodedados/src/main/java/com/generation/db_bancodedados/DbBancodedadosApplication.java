6b43deb46b25638a34245a55b209da113534786c
Dislani <dismelo16@gmail.com>
2022-08-04 22:46:02 -0300

    ATIVIDADE CRUD BACKEND BLOG-PESSOAL – PARTE 01

---
+++ db_bancodedados/src/main/java/com/generation/db_bancodedados/DbBancodedadosApplication.java
@@ -0,0 +1,13 @@
+package com.generation.db_bancodedados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbBancodedadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbBancodedadosApplication.class, args);
+	}
+
+}
