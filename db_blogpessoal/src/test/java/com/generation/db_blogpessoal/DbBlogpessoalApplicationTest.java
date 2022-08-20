861f426658391d5cc67c1c93c0fb03f6243b1f53
Dislani <dismelo16@gmail.com>
2022-08-19 21:43:49 -0300

    ATIVIDADE DEPLOY BACK-END

---
+++ db_blogpessoal/src/test/java/com/generation/db_blogpessoal/DbBlogpessoalApplicationTest.java
@@ -0,0 +1,13 @@
+package com.generation.db_blogpessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbBlogpessoalApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
