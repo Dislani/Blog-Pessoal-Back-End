fedef36a607079bbdf49e85553738cf55ebeb3b8
Dislani <dismelo16@gmail.com>
2022-09-11 11:24:55 -0300

    adição do PutMapping que não tinha sido adicionado  no temaController

--- src/main/java/com/generation/db_blogpessoal/controller/TemaController.java
+++ src/main/java/com/generation/db_blogpessoal/controller/TemaController.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.generation.db_blogpessoal.model.Tema;
 import com.generation.db_blogpessoal.repository.TemaRepository;
@@ -50,6 +52,12 @@ public class TemaController {
 	public ResponseEntity<Tema> post (@RequestBody Tema tema){
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(repository.save(tema));	
+	}
+
+	@PutMapping 
+	public ResponseEntity<Tema> put (@RequestBody Tema tema) {
+		return ResponseEntity.ok(repository.save(tema));
+		
 	}
 		
 	@DeleteMapping ("/{id}")
